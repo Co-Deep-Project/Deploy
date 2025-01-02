260f183a774ed5e024ddeb708b186550c914d0ba
seoin2744 <seoin9398@gmail.com>
2025-01-02 19:44:54 +0900

    chatbot server 배포 시도

--- backend/server.py
+++ backend/server.py
@@ -34,7 +34,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://backend-three-theta-46.vercel.app/"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
